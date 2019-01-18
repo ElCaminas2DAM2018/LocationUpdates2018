25528f9318497908d9b50a298e8c424ce3e2dde5
victor@ieselcaminas.org <victor@ieselcaminas.org>
2019-01-18 11:19:41 +0100

    Some cleaning

--- app/src/main/java/org/ieselcaminas/pmdm/locationupdates2018/MapsActivity.java
+++ app/src/main/java/org/ieselcaminas/pmdm/locationupdates2018/MapsActivity.java
@@ -7,16 +7,11 @@ import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
@@ -56,16 +51,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -91,11 +76,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
                     mLatitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
                     mLongitudeTextView.setText(String.valueOf(mCurrentLocation.getLongitude()));
-
                 }
             }
-
-            ;
         };
 
 
